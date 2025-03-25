be31497dc30c659f2c3cde673a8aa7e109b627f7
GabrielMoraisSL <gmoraisspmrx@gmail.com>
2025-03-25 01:37:19 -0300

    Atualização no titulo para responsividade em telas menores

--- src/App.jsx
+++ src/App.jsx
@@ -38,23 +38,25 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen bg-blue-500 justify-center items-center p-6 overflow-auto ">
-      <Title>Gerenciador de Tarefas</Title>
-      <div className="flex flex-col sm:flex-row justify-center p-3 gap-4 h-full ">
-        <div className="w-full md:w-[400px] space-y-4">
-          <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
-          <Tasks
-            tasks={tasks}
-            onTaskMoveClick={onTaskMoveClick}
-            onDeleteClick={onDeleteClick}
-          />
-        </div>
-        <div className="w-full md:w-[400px]">
-          <Completes
-            Completedtasks={completedTasks}
-            onCompletedTaskClick={onCompletedTaskClick}
-            onDeleteClick={onDeleteClick}
-          />
+    <div className="w-screen h-screen bg-blue-500 flex flex-col items-center p-6 overflow-auto">
+      <div>
+        <Title>Gerenciador de Tarefas</Title>
+        <div className="flex flex-col sm:flex-row justify-center p-5 gap-4 h-full ">
+          <div className="w-full md:w-[400px] space-y-4">
+            <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
+            <Tasks
+              tasks={tasks}
+              onTaskMoveClick={onTaskMoveClick}
+              onDeleteClick={onDeleteClick}
+            />
+          </div>
+          <div className="w-full md:w-[400px]">
+            <Completes
+              Completedtasks={completedTasks}
+              onCompletedTaskClick={onCompletedTaskClick}
+              onDeleteClick={onDeleteClick}
+            />
+          </div>
         </div>
       </div>
     </div>
