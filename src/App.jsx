b1c6d4f69c89a462d4d49d24a5ea533d2350032c
GabrielMoraisSL <gmoraisspmrx@gmail.com>
2025-03-25 23:19:30 -0300

    Alteração no layout

--- src/App.jsx
+++ src/App.jsx
@@ -41,21 +41,25 @@ function App() {
     <div className="w-screen h-screen bg-blue-500 flex flex-col items-center p-6 overflow-auto">
       <div>
         <Title>Gerenciador de Tarefas</Title>
-        <div className="flex flex-col sm:flex-row justify-center p-5 gap-4 h-full ">
-          <div className="w-full md:w-[400px] space-y-4">
-            <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
-            <Tasks
-              tasks={tasks}
-              onTaskMoveClick={onTaskMoveClick}
-              onDeleteClick={onDeleteClick}
-            />
-          </div>
-          <div className="w-full md:w-[400px]">
-            <Completes
-              completedTasks={completedTasks}
-              onCompletedTaskClick={onCompletedTaskClick}
-              onDeleteClick={onDeleteClick}
-            />
+        <div className="">
+          <div className="flex flex-col md:flex-row justify-center p-5 gap-4 h-full container">
+            <div className="w-full md:w-[400px] space-y-4">
+              <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
+            </div>
+            <div className="w-full md:w-[400px] space-y-4">
+              <Tasks
+                tasks={tasks}
+                onTaskMoveClick={onTaskMoveClick}
+                onDeleteClick={onDeleteClick}
+              />
+            </div>
+            <div className="w-full md:w-[400px]">
+              <Completes
+                completedTasks={completedTasks}
+                onCompletedTaskClick={onCompletedTaskClick}
+                onDeleteClick={onDeleteClick}
+              />
+            </div>
           </div>
         </div>
       </div>
